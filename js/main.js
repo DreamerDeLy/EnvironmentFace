0ad983a90927edc7a5786d895b0dc18d10681f0d
DreamerDeLy <dreamerandprogrammer@gmail.com>
2023-02-15 18:00:48 +0200

    Add loading screen

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 
+function getE(name){
+	return document.getElementById(name);
+}
 
+function getEc(name){
+	return document.getElementsByClassName(name);
+}
+
+function hideLoading() {
+	setTimeout(() => { getE("loading").style.opacity = "0"; }, 1000)
+	setTimeout(() => { getE("loading").style.display = "none"; }, 3000)
+}
+hideLoading();
