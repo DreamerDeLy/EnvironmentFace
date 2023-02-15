e86b01c3bf95acf97d8826ed3183621221526f85
DreamerDeLy <dreamerandprogrammer@gmail.com>
2023-02-15 18:01:29 +0200

    Add loading data from back-end (JSON)

--- js/main.js
+++ js/main.js
@@ -11,4 +11,94 @@ function hideLoading() {
 	setTimeout(() => { getE("loading").style.opacity = "0"; }, 1000)
 	setTimeout(() => { getE("loading").style.display = "none"; }, 3000)
 }
+
+function esc(str) {
+	if(str){
+		return str.toString()
+			.replace(/&/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/\"/g, '&quot;')
+			.replace(/\'/g, '&#39;')
+			.replace(/\//g, '&#x2F;');
+	}
+	return "";
+}
+
+function convertLineBreaks(str){
+	if(str){
+		str = str.toString();
+		str = str.replace(/(?:\r\n|\r|\n)/g,'<br>');
+		return str;
+	}
+	return "";
+}
+
+function getFile(adr, callback, timeout, method, onTimeout, onError){
+	/* fallback stuff */
+	if(adr === undefined) return;
+	if(callback === undefined) callback = function(){};
+	if(timeout === undefined) timeout = 8000; 
+	if(method === undefined) method = "GET";
+	if(onTimeout === undefined) {
+		onTimeout = function(){
+			console.error("ERROR: timeout loading file "+adr);
+		};
+	}
+	if(onError === undefined){
+		onError = function(){
+			console.error("ERROR: loading file: "+adr);
+		};
+	}
+	
+	/* create request */
+	var request = new XMLHttpRequest();
+	
+	/* set parameter for request */
+	request.open(method, encodeURI(adr), true);
+	request.timeout = timeout;
+	request.ontimeout = onTimeout;
+    request.onerror = onError;
+	request.overrideMimeType("application/json");
+	
+	request.onreadystatechange = function() {
+		if(this.readyState == 4){
+			if(this.status == 200){
+				callback(this.responseText);
+			}
+		}
+	};
+	
+	/* send request */
+	request.send();
+	
+	console.log("getFile: " + adr);
+}
+
+function parseData(fileStr)
+{
+	console.log("parse data");
+	var dataJson = JSON.parse(fileStr);
+	var elements = document.querySelectorAll("[data-replace]");
+
+	for (i = 0; i < elements.length; i++) 
+	{
+		var element = elements[i];
+		element.innerHTML = convertLineBreaks(esc(dataJson[element.getAttribute("data-replace")]));
+	}
+
+	// if (typeof load !== 'undefined') load();
+}
+
+getFile("/data/info.json",
+	parseData,
+	2000,
+	"GET",
+	function () {
+		getFile("/data/info.json", parseData);
+	}, function () {
+		getFile("/data/info.json", parseData);
+	}
+);
+
 hideLoading();
