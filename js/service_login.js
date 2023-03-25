5fe600785554c0b45dba6f66a82b0ef6d88ff4d9
Dreamer <dreamerandprogrammer@gmail.com>
2023-03-25 13:59:43 +0200

    Add loading info on service login page

--- js/service_login.js
+++ js/service_login.js
@@ -1,5 +1,26 @@
 import * as u from "./utils.js";
 
+
+function parseInfo(json) {
+	var data = JSON.parse(json);
+
+	// Fill info 
+	u.getE("id").innerText = data["id"];
+	u.getE("version").innerText = data["software-version"];
+}
+
+// Load info data
+u.getFile("/data/info.json",
+	parseInfo,
+	2000,
+	"GET",
+	function () {
+		u.getFile("/data/info.json", parseInfo);
+	}, function () {
+		u.getFile("/data/info.json", parseInfo);
+	}
+);
+
 b_paste.onclick = e => {
 	try {
 		navigator.clipboard.readText().then((clipText) =>
